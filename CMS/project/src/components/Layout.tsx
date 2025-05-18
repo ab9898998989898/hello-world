d3bda53780c114896bc1c11bf16d4391281910f0
ab9898998989898 <abdullahnadeem2580@gmail.com>
2025-05-18 22:57:29 +0500

    Delete CMS/project directory

--- CMS/project/src/components/Layout.tsx
+++
@@ -1,6 +0,0 @@
-// Previous Layout.tsx content with updated handleProfileClick function
-const handleProfileClick = () => {
-  setUserMenuOpen(false);
-  navigate('/profile');
-}; 
-export default Layout;
